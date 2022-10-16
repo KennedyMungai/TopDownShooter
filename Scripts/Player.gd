73ff668cb041e66bd95302131523a9683d865b84
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-10-16 17:08:11 +0300

    Added logic for horizontal motion

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -11,9 +11,18 @@ func _process(delta: float) -> void:
 
 
 func update_motion(delta: float) -> void:
+#	Vertical Motion
 	if(Input.is_action_pressed("ui_up") and not Input.is_action_pressed("ui_down")):
 		motion.y = clamp((motion.y - SPEED), -MAX_SPEED, 0);
 	elif(Input.is_action_pressed("ui_down") and not Input.is_action_pressed("ui_up")):
 		motion.y = clamp((motion.y + SPEED),0 ,MAX_SPEED);
 	else:
 		motion.y = lerp(motion.y, 0, FRICTION);
+		
+#	Horizontal Motion
+	if(Input.is_action_pressed("ui_left") and not Input.is_action_pressed("ui_right")):
+		motion.x = clamp((motion.x - SPEED), -MAX_SPEED, 0);
+	elif(Input.is_action_pressed("ui_right") and not Input.is_action_pressed("ui_left")):
+		motion.x = clamp((motion.x + SPEED),0 ,MAX_SPEED);
+	else:
+		motion.x = lerp(motion.x, 0, FRICTION);
