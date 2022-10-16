dab9098a1d5168e7a9a81fb609aff74881698a44
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-10-16 16:58:07 +0300

    Updated the motin code

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -11,4 +11,7 @@ func _process(delta: float) -> void:
 
 
 func update_motion(delta: float) -> void:
-	pass;
+	if(Input.is_action_pressed("ui_up") and not Input.is_action_pressed("ui_down")):
+		motion.y = clamp((motion.y - SPEED), -MAX_SPEED, 0);
+	if(Input.is_action_pressed("ui_down") and not Input.is_action_pressed("ui_up")):
+		motion.y = clamp((motion.y + SPEED),0 ,MAX_SPEED);
