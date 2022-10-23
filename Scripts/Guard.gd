4e78eb225d4c91d84add29d1d91d3bee8df93c88
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-10-23 20:52:52 +0300

    Declared some variables inside the Guard file

--- Scripts/Guard.gd
+++ Scripts/Guard.gd
@@ -1,3 +1,10 @@
 extends "res://Scripts/PlayerDetection.gd"
 
 
+# Variables
+var motion: Vector2 = Vector2();
+var possible_destination: Array = [];
+var path: Array = [];
+var destination: Vector2 = Vector2();
+
+
