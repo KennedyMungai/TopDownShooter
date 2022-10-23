8e25cbb92c7862378d49d1613042360c8430a7a9
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-10-23 12:04:27 +0300

    Used vectors to check the direction the player is facing

--- Scripts/PlayerDetection.gd
+++ Scripts/PlayerDetection.gd
@@ -14,3 +14,4 @@ func _process(delta: float) -> void:
 	
 func Player_is_in_FOV_TOLERANCE() -> void:
 	var NPC_facing_direction = Vector2(1, 0).rotated(global_rotation);
+	var direction_to_Player = Player.position - global_position;
