8b97a8e38dccbba12fb0543108e40cbba2f959a1
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-10-25 20:42:28 +0300

    Added the Canvas Modulate node to the interface group

--- Scripts/VisionMode.gd
+++ Scripts/VisionMode.gd
@@ -5,11 +5,13 @@ const DARK = Color("18147a");
 const NIGHTVISION = Color("35b088");
 
 func _ready() -> void:
+	add_to_group("interface");
 	color = DARK;
 	
 
 func NightVision() -> void:
 	color = NIGHTVISION;
+	
 	
 func DarkVision() -> void:
 	color = DARK;
