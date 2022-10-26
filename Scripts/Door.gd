33b5b14870c3db408a050becdb6ae029b6f3dd0c
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-10-26 20:59:37 +0300

    Set the can_click boolean to true on the else block

--- Scripts/Door.gd
+++ Scripts/Door.gd
@@ -8,6 +8,8 @@ var can_click: bool = false
 func _on_Door_body_entered(body: Node) -> void:
 	if body == Global.Player and not $AnimationPlayer.is_playing():
 		open();
+	else:
+		can_click = true;
 
 
 func _on_Door_body_exited(body: Node) -> void:
