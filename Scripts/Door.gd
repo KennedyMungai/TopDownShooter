ce2339cfbfb16fc8d10eb69b107d3fd92e09b663
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-10-26 20:54:06 +0300

    Set up the body exited signal

--- Scripts/Door.gd
+++ Scripts/Door.gd
@@ -7,3 +7,7 @@ var can_click: bool = false
 
 func _on_Door_body_entered(body: Node) -> void:
 	pass # Replace with function body.
+
+
+func _on_Door_body_exited(body: Node) -> void:
+	pass # Replace with function body.
