c44a12311b52d0bdf60e648ba6958f495ac9f1ab
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-10-27 21:56:23 +0300

    Used the propr subnode notation

--- Scripts/LockedDoor.gd
+++ Scripts/LockedDoor.gd
@@ -3,4 +3,4 @@ extends "res://Scripts/Door.gd"
 
 func _input_event(viewport: Object, event: InputEvent, shape_idx: int) -> void:
 	if Input.is_mouse_button_pressed(BUTTON_LEFT) and can_click:
-		$CanvasLayer.NumberPad.popup_centered();
+		$CanvasLayer/NumberPad.popup_centered();
