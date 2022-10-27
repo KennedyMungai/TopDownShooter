9387efac9b9c083722108503b18202b1342966d7
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-10-27 22:15:10 +0300

    The buttons are connected when the function starts

--- Scripts/NumberPad.gd
+++ Scripts/NumberPad.gd
@@ -1,6 +1,10 @@
 extends Popup
 
 
+func _ready() -> void:
+	connect_buttons();
+
+
 func connect_buttons() -> void:
 	for button in $Background/VSplitContainer/ButtonsContainer/ButtonGrid.get_children():
 		button.connect("pressed", self, "_on_Button_Pressed", [button.text]);
