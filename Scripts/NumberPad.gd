e2a02ab63f3cee9a6671639b3b88a205d18d3e09
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-10-27 22:19:51 +0300

    Created the _on_Button_Pressed function

--- Scripts/NumberPad.gd
+++ Scripts/NumberPad.gd
@@ -9,3 +9,10 @@ func connect_buttons() -> void:
 	for button in $Background/VSplitContainer/ButtonsContainer/ButtonGrid.get_children():
 		if button is Button:
 			button.connect("pressed", self, "_on_Button_Pressed", [button.text]);
+
+
+func _on_Button_Pressed(button) -> void:
+	if button == "Ok":
+		check_guess();
+	else:
+		enter(int(button));
