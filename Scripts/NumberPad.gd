afeb5c14957dfa8041f6003c1b8d9f4dc239e1ba
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-10-27 22:48:50 +0300

    Changed from red_light to green_light

--- Scripts/NumberPad.gd
+++ Scripts/NumberPad.gd
@@ -28,7 +28,7 @@ func _on_Button_Pressed(button) -> void:
 
 func check_guess() -> void:
 	if guess == combination:
-		light.texture = load(Global.red_light);
+		light.texture = load(Global.green_light);
 		$Timer.start();
 	else:
 		reset_lock();
