3de9c25c9db2ee69b44e062204059b3353152ff3
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-10-27 21:52:54 +0300

    Added a popup command for when the locked door is clicked

--- Scripts/LockedDoor.gd
+++ Scripts/LockedDoor.gd
@@ -1,3 +1,6 @@
 extends "res://Scripts/Door.gd"
 
 
+func _input_event(viewport: Object, event: InputEvent, shape_idx: int) -> void:
+	if Input.is_mouse_button_pressed(BUTTON_LEFT) and can_click:
+		$NumberPad.popup();
