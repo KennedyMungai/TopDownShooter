0b5c70b50664bf0b4dd6ad5d9acb0b826999890c
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-10-27 22:14:22 +0300

    Created the connect_buttons function that dynamically wires signals to buttons inside the UI

--- Scripts/NumberPad.gd
+++ Scripts/NumberPad.gd
@@ -1,3 +1,6 @@
 extends Popup
 
 
+func connect_buttons() -> void:
+	for button in $Background/VSplitContainer/ButtonsContainer/ButtonGrid.get_children():
+		button.connect("pressed", self, "_on_Button_Pressed", [button.text]);
