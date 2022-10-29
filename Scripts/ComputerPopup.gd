032b141132f3e003a53e109b60d4be776ddc5a0a
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-10-29 12:38:19 +0300

    Added the RichLabel text

--- Scripts/ComputerPopup.gd
+++ Scripts/ComputerPopup.gd
@@ -2,4 +2,4 @@ extends Popup
 
 
 func set_text(combination) -> void:
-	$RichTextLabel.bbcode_text = "Forgotten the access code. The access code is " + " but this is the last time";
+	$RichTextLabel.bbcode_text = ("Will you stop forgetting the access code? I've set it to " + PoolStringArray(combination).join("") + ", but this is the last time");
