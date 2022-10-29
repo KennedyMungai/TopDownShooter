75d516657c1ac7d1e61a8050a16b76908b85cb6a
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-10-29 11:12:16 +0300

    Removed the export varoable, the ready function and the print statement inside the generator_combination function

--- Scripts/CombinationGenerator.gd
+++ Scripts/CombinationGenerator.gd
@@ -1,14 +1,6 @@
 extends Node
 
 
-# Export vars
-export var length: int = 4; 
-
-
-func _ready() -> void:
-	generator_combination(length);
-
-
 func generator_combination(length: int) -> void:
 	var combination: Array = [];
 	
@@ -16,4 +8,3 @@ func generator_combination(length: int) -> void:
 		randomize();
 		combination.append(randi() % 10);
 		
-	print(combination);
