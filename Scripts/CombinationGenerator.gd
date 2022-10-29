adb30050afd89518b40837220e8255ffd5f194a4
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-10-29 11:13:07 +0300

    Returned the combination

--- Scripts/CombinationGenerator.gd
+++ Scripts/CombinationGenerator.gd
@@ -1,10 +1,11 @@
 extends Node
 
 
-func generator_combination(length: int) -> void:
+func generator_combination(length: int) -> Array:
 	var combination: Array = [];
 	
 	for number in range():
 		randomize();
 		combination.append(randi() % 10);
 		
+	return combination;
