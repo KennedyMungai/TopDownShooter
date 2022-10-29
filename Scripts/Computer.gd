df4aca1ac8431f4db62811f913e50a955fd38ac9
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-10-29 12:06:34 +0300

    Enabled the computer light when the player walks in to the scene and disabled it when the player leaves

--- Scripts/Computer.gd
+++ Scripts/Computer.gd
@@ -11,7 +11,9 @@ func _ready() -> void:
 
 func _on_Computer_body_entered(body: Node) -> void:
 	can_click = true;
+	$Light2D.enabled = true;
 
 
 func _on_Computer_body_exited(body: Node) -> void:
 	can_click = false;
+	$Light2D.enabled = false;
