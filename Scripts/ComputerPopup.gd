2d89c889a57ca5545d405b98c0f41833bcc94b50
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-10-29 12:40:27 +0300

    Changed up how the text is formatted

--- Scripts/ComputerPopup.gd
+++ Scripts/ComputerPopup.gd
@@ -2,4 +2,6 @@ extends Popup
 
 
 func set_text(combination) -> void:
-	$RichTextLabel.bbcode_text = ("Will you stop forgetting the access code? I've set it to " + PoolStringArray(combination).join("") + ", but this is the last time");
+	$RichTextLabel.bbcode_text = ("Will you stop forgetting the access code? I've set it to " 
+			+ PoolStringArray(combination).join("") + 
+			", but this is the last time");
