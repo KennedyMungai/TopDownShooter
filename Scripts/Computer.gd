4711a3bb765912581378a86aa4ea0ed55e131792
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-10-29 12:04:11 +0300

    Added the same variables back

--- Scripts/Computer.gd
+++ Scripts/Computer.gd
@@ -1,5 +1,17 @@
 extends Area2D
 
 
+var can_click: bool = false;
+var combination: Array;
+
+
 func _ready() -> void:
 	$Light2D.enabled = false;
+
+
+func _on_Computer_body_entered(body: Node) -> void:
+	pass # Replace with function body.
+
+
+func _on_Computer_body_exited(body: Node) -> void:
+	pass # Replace with function body.
