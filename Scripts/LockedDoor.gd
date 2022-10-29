55935132382f8dc50eeead3f4824715cd49f01e9
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-10-29 10:01:11 +0300

    Created the combination var

--- Scripts/LockedDoor.gd
+++ Scripts/LockedDoor.gd
@@ -1,6 +1,10 @@
 extends "res://Scripts/Door.gd"
 
 
+# var
+var combination: Array = [4, 1, 5];
+
+
 func _input_event(viewport: Object, event: InputEvent, shape_idx: int) -> void:
 	if Input.is_mouse_button_pressed(BUTTON_LEFT) and can_click:
 		$CanvasLayer/NumberPad.popup_centered();
