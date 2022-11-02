e8e0b4e0827df9b32e930da5a0551a270b3dead4
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-11-02 21:43:29 +0300

    Added the urls for the box sprite and the hit man sprite

--- Scripts/Global Scripts/Global.gd
+++ Scripts/Global Scripts/Global.gd
@@ -13,3 +13,6 @@ var nightvision_off_sfx = "res://SFX/nightvision_off.wav";
 
 var red_light = "res://GFX/Interface/PNG/dotRed.png";
 var green_light = "res://GFX/Interface/PNG/dotGreen.png";
+
+var box_sprite = "res://GFX/PNG/Tiles/tile_42.png";
+var player_sprite = "res://GFX/PNG/Hitman 1/hitman1_stand.png";
