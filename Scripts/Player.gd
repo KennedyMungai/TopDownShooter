f1febdd0210f2974a2636d2f7af631c1ed7b0671
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-11-03 20:04:05 +0300

    Set teh rect_rotation to show the label upright on every frame

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -28,7 +28,7 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	update_motion(delta);
 	move_and_slide(motion);
-	$Label.rect_rotation = 0;
+	$Label.rect_rotation = -rotation_degrees;
 
 
 func update_motion(delta: float) -> void:
