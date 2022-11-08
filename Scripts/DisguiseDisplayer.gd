00741c1bbb1a24150903b6a40245be567f523eea
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-11-08 20:57:35 +0300

    Made the GUI element unselectable

--- Scripts/DisguiseDisplayer.gd
+++ Scripts/DisguiseDisplayer.gd
@@ -5,4 +5,4 @@ func update_disguises(number: int) -> void:
 	clear(); 
 	
 	for disguises in range(number):
-		add_icon_item(load(Global.box_sprite))
+		add_icon_item(load(Global.box_sprite), false)
