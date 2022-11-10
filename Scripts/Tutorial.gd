3a344fb2496791d90e1ca2360423ee278cc4e814
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-11-10 20:32:39 +0300

    Added numbers as the parameters of the update_pointer_position function calls

--- Scripts/Tutorial.gd
+++ Scripts/Tutorial.gd
@@ -2,27 +2,27 @@ extends Node2D
 
 
 func _ready() -> void:
-	update_pointer_position()
+	update_pointer_position(0)
 
 
 func _on_ObjectiveMove_body_entered(body: Node) -> void:
-	update_pointer_position()
+	update_pointer_position(1)
 
 
 func _on_ObjectiveDoor_body_entered(body: Node) -> void:
-	update_pointer_position()
+	update_pointer_position(2)
 
 
 func _on_ObjectiveNightVision_body_entered(body: Node) -> void:
-	update_pointer_position()
+	update_pointer_position(3)
 
 
 func _on_ObjectiveLoot_body_entered(body: Node) -> void:
-	update_pointer_position()
+	update_pointer_position(4)
 
 
 func _on_ObjectiveExit_body_entered(body: Node) -> void:
-	update_pointer_position()
+	update_pointer_position(5)
 
 
 func update_pointer_position(number: int) -> void:	
