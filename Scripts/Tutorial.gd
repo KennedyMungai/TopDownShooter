471b4387b30397f53fbf5132ee960fb2c0241c05
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-11-10 19:59:36 +0300

    Attached all the area2D nodes to to the script via signals

--- Scripts/Tutorial.gd
+++ Scripts/Tutorial.gd
@@ -1,3 +1,22 @@
 extends Node2D
 
 
+
+func _on_ObjectiveMove_body_entered(body: Node) -> void:
+	pass # Replace with function body.
+
+
+func _on_ObjectiveDoor_body_entered(body: Node) -> void:
+	pass # Replace with function body.
+
+
+func _on_ObjectiveNightVision_body_entered(body: Node) -> void:
+	pass # Replace with function body.
+
+
+func _on_ObjectiveLoot_body_entered(body: Node) -> void:
+	pass # Replace with function body.
+
+
+func _on_ObjectiveExit_body_entered(body: Node) -> void:
+	pass # Replace with function body.
